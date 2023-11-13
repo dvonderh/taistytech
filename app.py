512c3d6b0697d11fee8def6bd6200344b9187632
Dylan Vonderhorst <dvonderh@student11.cse.nd.edu>
2023-11-13 11:45:43 -0500

    login

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 # app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, session
+from flask_oidc import OpenIDConnect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dvonderh'
+app.config['OIDC_CLIENT_SECRETS'] = 'client_secrets.json'  # Create this file with your Azure AD app info
+app.config['OIDC_ID_TOKEN_COOKIE_SECURE'] = False
+app.config['OIDC_REQUIRE_VERIFIED_EMAIL'] = False
+oidc = OpenIDConnect(app)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    if oidc.user_loggedin:
+        return 'Logged in as: ' + oidc.user_getfield('preferred_username')
+    return 'Not logged in!'
 
-if __name__ == "__main__":
+@app.route('/login')
+@oidc.require_login
+def login():
+    return redirect(url_for('.home'))
+
+@app.route('/logout')
+def logout():
+    oidc.logout()
+    return 'Logged out'
+
+if __name__ == '__main__':
     app.run(debug=True)
 
