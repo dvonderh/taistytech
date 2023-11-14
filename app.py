d930f5eb8462c783257ed301fb48c8710d02d74b
Dylan Vonderhorst <dvonderh@student11.cse.nd.edu>
2023-11-13 20:06:17 -0500

    login

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/login")
-def index():
+@app.route("/login/")
+def login():
     return render_template("login.html")
 
 if __name__ == "__main__":
