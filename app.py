d2353d26c32205e2c242df77c156083f9edef8cb
Dylan Vonderhorst <dvonderh@student11.cse.nd.edu>
2023-11-13 20:29:45 -0500

    db

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 # app.py
 from flask import Flask, render_template
+import pyodbc
 
 app = Flask(__name__)
+
+server = 'taistytech.database.windows.net'
+database = 'TaistyTech'
+username = 'dvonderh'
+password = '0527Butterfly!'
+driver = '{ODBC Driver 17 for SQL Server}'
 
 @app.route("/")
 def index():
@@ -10,6 +17,30 @@ def index():
 @app.route("/login/")
 def login():
     return render_template("login.html")
+
+# Database update route
+@app.route("/update-database", methods=['POST'])
+def update_database():
+    try:
+        # Extract the ingredient from the JSON data
+        ingredient = request.json.get('ingredient')
+
+        # Connect to the database
+        conn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}')
+        cursor = conn.cursor()
+
+        # Execute a SQL query to update the database
+        cursor.execute(f"INSERT INTO Ingredients (Name) VALUES ('{ingredient}')")
+        conn.commit()
+
+        # Close the database connection
+        conn.close()
+
+        # Respond with a success message
+        return jsonify({'message': 'Ingredient added to the database successfully'})
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == "__main__":
     app.run(debug=True)
