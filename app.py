0ba823c860c6c8e79c08554ee24bde7ab7de6aaa
Dylan Vonderhorst <dvonderh@student11.cse.nd.edu>
2023-12-05 14:08:58 -0500

    a

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+
+@app.route("/add_ingr", methods['GET', 'POST'])
+def add_ingr():
+    if requests.method == 'POST':
+        ingredient = request.form['ingredient']
+
 if __name__ == "__main__":
     app.run(debug=True)
 
