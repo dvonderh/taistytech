0367e8a7ff94c7174a1612fb71375c57dfd0814a
Dylan Vonderhorst <dvonderh@student11.cse.nd.edu>
2023-12-05 14:42:29 -0500

    please

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"]))
+@app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         ingredient = request.form["ingredient"]
