db0647de27e8086f92cd3bb94d04523971e0b967
Dylan Vonderhorst <dvonderh@student11.cse.nd.edu>
2023-12-06 12:46:34 -0500

    a

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # app.py
 from flask import Flask, render_template, request, jsonify
+from edemam import search_recipes
 
 app = Flask(__name__)
 
@@ -19,6 +20,11 @@ def index():
 def get_ingredients():
     if ingredients != []:
         return jsonify({'ingredients': ingredients})
+
+@app.route("/find_recipes", methods=["GET"])
+def find_recipes():
+    recipes=search_recipes(ingredients)
+    return jsonify({'recipes': recipes})
 
 if __name__ == '__main__':
     app.run(debug=True)
