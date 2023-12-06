ac0d0654beb50e11f8e5c22bf03a4e6922584fec
Dylan Vonderhorst <dvonderh@student11.cse.nd.edu>
2023-12-06 11:09:02 -0500

    TESTER

--- app.py
+++ app.py
@@ -1,38 +1,11 @@
 # app.py
-from flask import Flask, render_template, request, jsonify
-#from edemam import *
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-ingredients = []
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    pass
-    #if request.method == "POST":
-        #ingredient = request.form["ingredient"]
-        #if ingredient not in ingredients and ingredient != '':
-            #ingredients.append(ingredient)
- 
-    #return render_template("index.html", ingredient_list=ingredients)
-
-'''
-@app.route("/get_ingredients", methods=["GET"])
-def get_ingredients():
-    if ingredients != []:
-        return jsonify({'ingredients': ingredients})
-
-@app.route('/my-link', methods=["POST"])
-def findRecipes():
-    recipe = search_recipes(ingredients)
-    out = []
-    for i in recipe:
-        out.append(i['label'])
-        out.append(i['url'])
-    #return jsonify({'recipes': out})
-    return render_template("index.html", recipes=out)
- '''
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
-
