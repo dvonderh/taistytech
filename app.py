3a0db4fc4f05e9510be93ae674c7b003582d97f4
Dylan Vonderhorst <dvonderh@student11.cse.nd.edu>
2023-12-06 11:35:29 -0500

    a

--- app.py
+++ app.py
@@ -5,7 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        ingredient = request.form["ingredient"]
+        if ingredient not in ingredients and ingredient != '':
+            ingredients.append(ingredient)
+
+    return render_template("index.html", ingredient_list=ingredients)
+
+
+@app.route("/get_ingredients", methods=["GET"])
+def get_ingredients():
+    if ingredients != []:
+        return jsonify({'ingredients': ingredients})
 
 if __name__ == '__main__':
     app.run(debug=True)
